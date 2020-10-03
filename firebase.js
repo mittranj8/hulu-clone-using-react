81da74de784f2cb31f0c01f0d7b750500e6dfa0f
Mittranj <mpansuriya@unomaha.edu>
2020-10-03 14:26:43 -0500

    added intial firebase config file

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBncwDde2CoLwd4X-nXmMTOuEMxpLyWd2E",
+  authDomain: "hulu-clone-react-4424c.firebaseapp.com",
+  databaseURL: "https://hulu-clone-react-4424c.firebaseio.com",
+  projectId: "hulu-clone-react-4424c",
+  storageBucket: "hulu-clone-react-4424c.appspot.com",
+  messagingSenderId: "213539946322",
+  appId: "1:213539946322:web:e496cb243f5671486cc48b",
+  measurementId: "G-RK10T9GF27",
+};
